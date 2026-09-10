e73085f9613a7bb481bcc7d277e77027292b4dd7
Doshagyasing Gowardun <45894815+Redstoneinvente@users.noreply.github.com>
2026-09-10 09:05:32 +0400

    Enable artwork palette for album color styling

--- Halo/Core/WorkspaceStore.swift
+++ Halo/Core/WorkspaceStore.swift
@@ -49,7 +49,11 @@ final class WorkspaceStore: ObservableObject, LiveActivityProvider {
     }
     private func updateArtworkPreference() {
         let layouts = [effectiveLayout] + settings.displays.compactMap { $0.enabled ? $0.layout : nil }
-        media.setArtworkEnabled(layouts.contains { $0.closedNotch?.visualizer?.dynamicColors == true })
+        media.setArtworkEnabled(layouts.contains {
+            $0.closedNotch?.visualizer?.dynamicColors == true ||
+            $0.closedNotch?.albumTextColor == true ||
+            $0.closedNotch?.albumBackgroundColor == true
+        })
     }
     func start() {
         updateArtworkPreference()
