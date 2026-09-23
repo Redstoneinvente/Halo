af95f33b16d87729329c693b5ebcb67045d5b752
Doshagyasing Gowardun <45894815+Redstoneinvente@users.noreply.github.com>
2026-09-23 09:13:37 +0400

    Track adaptive Trailer transition duration

--- Halo/Core/WorkspaceStore.swift
+++ Halo/Core/WorkspaceStore.swift
@@ -66,6 +66,7 @@ final class WorkspaceStore: ObservableObject, LiveActivityProvider {
     @Published var trailerModeActive = false
     @Published var trailerTransitionActive = false
     @Published var trailerTransitionStyle: TrailerTransitionStyle = .fade
+    @Published var trailerTransitionDuration = 0.18
 
     var baseEffectiveLayout: WorkspaceLayout { settings.profiles.first { $0.id == scheduledProfileID }?.layout ?? settings.layout }
     var effectiveLayout: WorkspaceLayout { trailerLayoutOverride ?? baseEffectiveLayout }
